e224eebe82acb6e7bf710881e348adbaedffd3e1
thomas.plowman <thomas.plowman@grapeshot.co.uk>
2017-05-14 16:58:53 +0100

    Moving things around

---
+++ base_app/core.py
@@ -0,0 +1,8 @@
+from base_app import app
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
+
+db = SQLAlchemy(app)
+
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
---
+++ base_app/settings.py
@@ -0,0 +1,5 @@
+
+DEBUG = True
+SECRET_KEY = 'temporary_secret_key'  # make sure to change this
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://flask:b52l0bster@localhost:5432/base_app'
